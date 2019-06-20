2d0d37bf09784f983e9ac63cbe864ad8df3fb981
skyorion427 <skyorion427@gmail.com>
2019-06-20 14:58:40 +0800

    Update GraphQL query

--- src/graphql/uniswap.js
+++ src/graphql/uniswap.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export const QueryGetExchangeData = gql`
-  query GetExchangeData($first: String) {
-    exchanges(first: $first) {
+  query GetExchangeData($first: Int!, $skip: Int!) {
+    exchanges(first: $first, skip: $skip) {
       id
       ethBalance
     }
@@ -10,8 +10,8 @@ export const QueryGetExchangeData = gql`
 `;
 
 export const QueryGetUserTransaction = gql`
-  query GetUserTransaction($first: String, $where: String) {
-    exchanges(first: 1, where: $where) {
+  query GetUserTransaction($first: String, $id: String) {
+    exchanges(id: $id) {
       id
       txs(first: 5) {
         id
